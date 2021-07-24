ec87bc8c6d8a0d3890d475bf6802aa2d53105f23
Kritika <24kritikasharma24@gmail.com>
2021-07-24 15:21:20 +0530

    dynamically populating the feed

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,7 +1,8 @@
 import './post.css'
 import { MoreVert } from '@material-ui/icons'
+import { Users } from '../../dummyData'
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className='post'>
       <div className='post-wrapper'>
@@ -9,28 +10,32 @@ export default function Post() {
           <div className='post-top-left'>
             <img
               className='post-profile-img'
-              src='/assets/person/1.jpeg'
+              src={Users.filter((u) => u.id === post.userId)[0].profilePicture}
               alt=''
             />
-            <span className='post-user-name'>Susan James</span>
-            <span className='post-date'>8 mins ago</span>
+            <span className='post-user-name'>
+              {Users.filter((u) => u.id === post.userId)[0].username}
+            </span>
+            <span className='post-date'>{post.date}</span>
           </div>
           <div className='post-top-right'>
             <MoreVert />
           </div>
         </div>
         <div className='post-center'>
-          <span className='post-text'>Hey! It's my first post</span>
-          <img className='post-img' src='/assets/post/1.jpeg' alt='' />
+          <span className='post-text'>{post?.desc}</span>
+          <img className='post-img' src={post.photo} alt='' />
         </div>
         <div className='post-bottom'>
           <div className='post-bottom-left'>
             <img className='reaction-icon' src='/assets/like.png' alt='' />
             <img className='reaction-icon' src='/assets/heart.png' alt='' />
-            <span className='post-like-counter'>37 people like this</span>
+            <span className='post-like-counter'>
+              {post.like} people like this
+            </span>
           </div>
           <div className='post-bottom-right'>
-            <span className='post-comment-text'>8 comments</span>
+            <span className='post-comment-text'>{post.comment} comments</span>
           </div>
         </div>
       </div>
